201d7a1ebd70301d18255c16b7b6e8f825ddd614
Cyril Brouillard <cyril.brouillard@alsyon-tech.net>
2013-01-16 17:48:52 +0100

    Ajout de l'activité de simulation de lancé de dés.

---
+++ src/com/headbangers/reportmaker/DiceSimulationActivity.java
@@ -0,0 +1,19 @@
+package com.headbangers.reportmaker;
+
+import android.os.Bundle;
+import android.widget.GridView;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class DiceSimulationActivity extends SherlockActivity {
+
+	private GridView gridView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dice_simulation);
+
+		this.gridView = (GridView) findViewById(R.id.diceGrid);
+	}
+}
